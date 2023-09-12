d5e5aeacbc80f89ea536defc20fca5a4764212d0
Frank Wang <frankwaang@outlook.com>
2023-09-12 12:54:36 -0500

    fix: userIcon popover

--- src/components/buttons/userbutton.tsx
+++ src/components/buttons/userbutton.tsx
@@ -11,6 +11,7 @@ import {
 } from '@mantine/core';
 import { IconChevronRight } from '@tabler/icons-react';
 import { Link } from '@tanstack/react-router';
+import { useState } from 'react';
 
 const useStyles = createStyles((theme) => ({
   user: {
@@ -24,6 +25,8 @@ const useStyles = createStyles((theme) => ({
     },
   },
   link: {
+    paddingX: theme.spacing.lg,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
     '&:hover': {
       backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
       color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
@@ -40,12 +43,12 @@ interface UserButtonProps extends UnstyledButtonProps {
 
 export function UserButton({user, ...others }: UserButtonProps) {
   const { classes } = useStyles();
-
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <Popover >
+    <Popover opened={open}>
       <Popover.Target >
         {/* <UnstyledButton className={classes.user} /> */}
-          <div className={classes.user}>
+          <div className={classes.user} onClick={(o) => setOpen(true)}>
             <Group>
             <Avatar src={user.avatar} radius="xl" />
             <div style={{ flex: 1 }}>
@@ -63,12 +66,12 @@ export function UserButton({user, ...others }: UserButtonProps) {
       </Popover.Target>
       <Popover.Dropdown sx={(theme) => ({ background: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white })}>
         <Stack spacing="sm" sx={(theme) => ({backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white})}>
-        <Link to="/me" className={classes.link}>
+        <Link to="/me" className={classes.link} onClick={(o) => setOpen(false)}>
           <UnstyledButton>
             Me
           </UnstyledButton>
         </Link >
-        <Link to="/version" className={classes.link}>
+        <Link to="/version" className={classes.link} onClick={(o) => setOpen(false)}>
           <UnstyledButton component={SignOutButton}>
             Sign Out
           </UnstyledButton>
