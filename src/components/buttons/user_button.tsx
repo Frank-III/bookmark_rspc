f0533e28b99f175d89504daea4353763b7925f37
Frank Wang <1454884738@qq.com>
2023-09-19 15:21:06 -0500

    feat: add collection links ui

--- src/components/buttons/user_button.tsx
+++ src/components/buttons/user_button.tsx
@@ -4,6 +4,9 @@ import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 export function UserButton() {
   return (
     <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+      </DropdownMenu.Trigger>
+      
     </DropdownMenu.Root>
 
   )
