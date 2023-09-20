5782549fbcd9962bf2ac91c20b7d1ca055fea0f1
Frank Wang <1454884738@qq.com>
2023-09-20 16:19:01 -0500

    feat: add blur when no collections(wip)

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,6 +9,7 @@ import {
   Search,
   Loader2,
   Dot,
+  GalleryVerticalEnd,
 } from 'lucide-react';
 import React, {
   ForwardRefExoticComponent,
@@ -25,6 +26,14 @@ const privateLinks = [
   { href: '/collections', label: 'Collections', icon: <Boxes /> },
   { href: '/me', label: 'Me', icon: <User /> },
 ] as const;
+
+const fakedCollections = [
+  {color: "red", name: "test1", id: "1"},
+  {color: "blue", name: "test2", id: "2"},
+  {color: "green", name: "test3", id: "3"},
+  {color: "gray", name: "test4", id: "4"},
+]
+
 
 interface LinksProps {
   links: typeof privateLinks;
@@ -104,11 +113,42 @@ export function Nav() {
         return (
           <>
             {collections.length == 0 ? (
-              <div className='flex items-center justify-center mt-5'>
-                <span className='text-gray-700 text-sm '>
-                  You have no Pinned Collections
-                </span>
-              </div>
+              // <div className='flex items-center justify-center mt-5'>
+              //   <span className='text-gray-700 text-sm '>
+              //     You have no Pinned Collections
+              //   </span>
+              // </div>
+                <>
+                  <div className=" pointer-events-none opacity-40 blur-sm">
+                    {fakedCollections.map((collection) => (
+                      <a href="#">
+                        <button className='group flex w-full flex-row items-center justify-between rounded-lg border-2 border-transparent px-2 py-1 transition font-medium text-gray-600 hover:bg-gray-100 hover:text-gray-900'>
+                          <div className='flex flex-row items-center justify-start truncate'>
+                            <div className='mr-1.5 flex h-5 w-5 items-center justify-center'>
+                              <Dot color={collection.color} size={'auto'} />
+                            </div>
+                            <p className='truncate text-sm'>{collection.name}</p>
+                          </div>
+                        </button>
+                      </a>
+                    )
+                    )}
+                  </div>
+                  <div className='absolute h-full w-full bg-gradient-to-t from-white to-transparent'>
+                  </div>
+                  <div className='absolute left-1/2 top-6 mx-auto flex max-w-[150px] -translate-x-1/2 flex-col items-center justify-center text-center'>
+                    <GalleryVerticalEnd size={24}/>
+                    <h4 className='mb-1 mt-3 text-sm font-medium text-gray-900'>No Collections</h4>
+                    <p className='mb-3 text-xs text-gray-500'>Create a collection to organize your writing.</p>
+                    <div className='mx-auto'>
+                      <button className='bg-white text-gray-700 focus:outline-none ring-1 ring-black/5 shadow-small hover:bg-gray-50 disabled:text-gray-400 disabled:hover:bg-white focus-visible:button-focus-outline  auto shrink-0 h-8 rounded-lg px-2 text-sm min-w-[60px] relative flex flex-row items-center justify-center space-x-1 font-medium transition disabled:cursor-not-allowed'>
+                        <span className='relative whitespace-nowrap'>
+                          Create Collection
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </>
             ) : (
               collections?.map((collection) => {
                 const { id, name, color } = collection;
@@ -163,7 +203,7 @@ export function Nav() {
         <div className='relative flex w-full items-center justify-center mt-4 px-2'>
           <div className='h-[1px] w-full rounded-full bg-gray-100' />
         </div>
-        <div className='styled-scrollbar relative flex w-full flex-auto flex-col space-y-2 overflow-y-auto px-2 pb-2 pt-2'>
+        <div className='styled-scrollbar relative flex w-full flex-auto flex-col space-y-2 overflow-x-hidden overflow-y-auto px-2 pb-2 pt-2'>
           <div className='sticky -top-2 z-10 flex w-full flex-row items-center justify-between px-2 pt-2'>
             <div className='pointer-events-none absolute left-0 top-0 z-0 h-10 w-full bg-gradient-to-b from-white via-white'></div>
             <p className='relative z-10 text-xs font-medium text-gray-500'>
