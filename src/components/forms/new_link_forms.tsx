44370c69104ea6a1b8c275179f77fabf1534416f
Frank Wang <frankwaang@outlook.com>
2023-09-21 23:21:22 -0500

    form works

--- src/components/forms/new_link_forms.tsx
+++ src/components/forms/new_link_forms.tsx
@@ -18,7 +18,7 @@ import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { CaretSortIcon } from '@radix-ui/react-icons';
 import { cn } from '../../utils';
 import { Command as CommandPrimative } from 'cmdk';
-import { CheckIcon, Command } from 'lucide-react';
+import { CheckIcon, Command, Search } from 'lucide-react';
 import {
   CommandList,
   CommandEmpty,
@@ -29,11 +29,11 @@ import {
 
 export function NewLinkForm() {
   const queryClient = rspc.useContext().queryClient;
-  const { data: potentialCollections, isLoading: collectionLoading } = rspc.useQuery([
-    'collections.getByUser'], {
+  const { data: potentialCollections, isLoading: collectionLoading } =
+    rspc.useQuery(['collections.getByUser'], {
       onSuccess: () => {
         console.log(potentialCollections);
-      }
+      },
     });
   const addLink = rspc.useMutation(['links.create'], {
     onSuccess: () => {
@@ -79,15 +79,11 @@ export function NewLinkForm() {
     });
   }
 
-  if (collectionLoading) {
-    return <div>Loading...</div>;
-  }
-
   // 3. Render the form.
   // TODO: a select for collections then get id from that
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='py-3'>
         <FormField
           control={form.control}
           name='link_name'
@@ -97,7 +93,7 @@ export function NewLinkForm() {
               <FormControl>
                 <Input placeholder='Link Name' {...field} />
               </FormControl>
-              <FormDescription>This is the link name</FormDescription>
+              {/* <FormDescription>This is the link name</FormDescription> */}
               <FormMessage />
             </FormItem>
           )}
@@ -111,7 +107,7 @@ export function NewLinkForm() {
               <FormControl>
                 <Input placeholder='Url' {...field} />
               </FormControl>
-              <FormDescription>Url to the link</FormDescription>
+              {/* <FormDescription>Url to the lin</FormDescription> */}
               <FormMessage />
             </FormItem>
           )}
@@ -125,7 +121,6 @@ export function NewLinkForm() {
               <FormControl>
                 <Input placeholder='Discription' {...field} />
               </FormControl>
-              <FormDescription>Url to the link</FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -134,7 +129,7 @@ export function NewLinkForm() {
           control={form.control}
           name='collection_id'
           render={({ field }) => (
-            <FormItem className='flex flex-col'>
+            <FormItem className='py-3 flex flex-col'>
               <FormLabel>Collection</FormLabel>
               <Popover>
                 <PopoverTrigger asChild>
@@ -143,7 +138,7 @@ export function NewLinkForm() {
                       variant='outline'
                       role='combobox'
                       className={cn(
-                        'w-[200px] justify-between',
+                        'justify-between',
                         !field.value && 'text-muted-foreground',
                       )}
                     >
@@ -151,52 +146,63 @@ export function NewLinkForm() {
                         ? potentialCollections?.find(
                             (collection) => collection.id === field.value,
                           )?.name
-                        : 'Select language'}
+                        : 'Select collection'}
                       <CaretSortIcon className='ml-2 h-4 w-4 shrink-0 opacity-50' />
                     </Button>
                   </FormControl>
                 </PopoverTrigger>
-                <PopoverContent className='w-[200px] p-0'>
-                  <Command>
-                      <CommandInput placeholder='Search language...' />
-                      {collectionLoading && (
-                        <CommandPrimative.Loading>
-                          Loading...
-                        </CommandPrimative.Loading>
-                      )}
-                      <CommandEmpty>No language found.</CommandEmpty>
-                      <CommandGroup>
-                        {potentialCollections?.map((collection) => (
-                          <CommandItem
-                            value={collection.id.toString()}
-                            key={`collections+${collection.name}`}
-                            onSelect={() => {
-                              form.setValue('collection_id', collection.id);
-                            }}
-                          >
-                            <CheckIcon
-                              className={cn(
-                                'mr-2 h-4 w-4',
-                                collection.id === field.value
-                                  ? 'opacity-100'
-                                  : 'opacity-0',
-                              )}
-                            />
-                            {collection.name}
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                  </Command>
+                <PopoverContent className='w-[400px] p-0'>
+                  <CommandPrimative className='flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground'>
+                    <div
+                      className='flex items-center border-b px-3'
+                      cmdk-input-wrapper=''
+                    >
+                      <Search className='mr-2 h-4 w-4 shrink-0 opacity-50' />
+                      <CommandPrimative.Input
+                        placeholder='Search Collection...'
+                        className='flex h-11 rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50'
+                      />
+                    </div>
+                    {collectionLoading && (
+                      <CommandPrimative.Loading>
+                        Loading...
+                      </CommandPrimative.Loading>
+                    )}
+                    <CommandPrimative.Empty className='py-6 text-center text-sm'>
+                      Collection not found.
+                    </CommandPrimative.Empty>
+                    <CommandPrimative.Group className='overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground'>
+                      {potentialCollections?.map((collection) => (
+                        <CommandPrimative.Item
+                          value={collection.id.toString()}
+                          key={`collections+${collection.name}`}
+                          className='relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50'
+                          onSelect={() => {
+                            form.setValue('collection_id', collection.id);
+                          }}
+                        >
+                          <CheckIcon
+                            className={cn(
+                              'mr-2 h-4 w-4',
+                              collection.id === field.value
+                                ? 'opacity-100'
+                                : 'opacity-0',
+                            )}
+                          />
+                          {collection.name}
+                        </CommandPrimative.Item>
+                      ))}
+                    </CommandPrimative.Group>
+                  </CommandPrimative>
                 </PopoverContent>
               </Popover>
-              <FormDescription>
-                The collection you want to add the link to
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type='submit'>Add Link</Button>
+        <Button type='submit' className='mt-3'>
+          Add Link
+        </Button>
       </form>
     </Form>
   );
