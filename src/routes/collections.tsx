84b1a600b2986c5225f617e39e541b6f1f972dc1
Frank Wang <frankwaang@outlook.com>
2023-09-23 20:13:18 -0500

    make edit collection modal work

--- src/routes/collections.tsx
+++ src/routes/collections.tsx
@@ -2,9 +2,23 @@ import * as React from 'react';
 import { FileRoute } from '@tanstack/react-router';
 import { NewCollection } from '../components/modals/collection_modals';
 import { Button } from '../components/ui/button';
+import { CollectionDropdown, DropdownWithDialogItemsSolution2 } from '../components/buttons/collection_popover';
+import { rspc } from '../utils/rspc';
+import { CollectionPinned } from '../components/links/collection_links';
 
 export const route = new FileRoute('/collections').createRoute({
   component: () => {
+    const {isLoading, data: collections} = rspc.useQuery(['collections.getAllWithPinned']);
+    if (isLoading) {
+      return <div>Error</div>
+    }
+    
+    const collects = collections?.map((c) => {
+        const {pinnedBy, ...collection} = c;
+        return {...collection, isPinned: pinnedBy.length > 0 ? true : false} as CollectionPinned;
+    });
+
+
     return (
       <>
         <div className='w-full bg-base-200'>
@@ -13,6 +27,7 @@ export const route = new FileRoute('/collections').createRoute({
             <NewCollection>
               <Button variant='outline'>New Collection</Button>
             </NewCollection>
+            {/* <DropdownWithDialogItemsSolution2 collection={collects[0]}/> */}
           </div>
         </div>
       </>
