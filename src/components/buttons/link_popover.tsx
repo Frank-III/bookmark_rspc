6819961b87a3be334407ef33157de70f8d475321
Frank Wang <frankwaang@outlook.com>
2023-09-25 19:09:28 -0500

    add new popover

---
+++ src/components/buttons/link_popover.tsx
@@ -0,0 +1,83 @@
+
+import { MoreVertical } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuGroup,
+} from '../ui/dropdown-menu';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+  DialogDescription,
+} from '../ui/dialog';
+import { DialogHeader } from '../ui/dialog';
+import React from 'react';
+import { LinkWithTags } from '../../../bindings';
+import { EditLinkForm } from '../forms/edit_link_forms';
+
+interface CollectionPopoverProps {
+  link: LinkWithTags;
+  children: React.ReactNode;
+}
+
+export function CollectionDropdown({
+  link,
+  children,
+}: CollectionPopoverProps) {
+  const [open, setOpen] = React.useState(false);
+  const [showEdit, setShowEdit] = React.useState(false);
+  return (
+    <Dialog open={showEdit} onOpenChange={setShowEdit}>
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+        <DropdownMenuContent className='w-[220px]'>
+          <DropdownMenuItem>
+            <DialogTrigger
+              asChild
+              onSelect={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <button
+                onClick={(e) => {
+                  setOpen(false);
+                  setShowEdit(true);
+                  e.preventDefault();
+                }}
+              >
+                Edit Link
+              </button>
+            </DialogTrigger>
+          </DropdownMenuItem>
+          <DropdownMenuItem>Remove Pinned</DropdownMenuItem>
+          <DropdownMenuItem
+            className='data-[highlighted]:bg-red-500 data-[highlighted]:opacity-80 data-[highlighted]:text-black'
+            key='delete'
+          >
+            Delete Collection
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel className='text-xs text-gray-700 font-light'>
+            Deleting a collection does not remove any of its content.
+          </DropdownMenuLabel>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DialogContent className='sm:max-w-[350px]'>
+        <DialogHeader>
+          <DialogTitle className='text-gray-800 font-md'>
+            Edit Collection
+          </DialogTitle>
+        </DialogHeader>
+        <div className='h-[0.8px] w-full rounded-full bg-gray-200' />
+        <EditLinkForm link={link} />
+      </DialogContent>
+    </Dialog>
+  );
+}
