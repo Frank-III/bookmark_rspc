7bb4831534e9a130d859b52df6dd92cbd73f6568
Frank Wang <frankwaang@outlook.com>
2023-09-27 01:04:50 -0500

    add calendar view

--- src/components/calender_view.tsx
+++ src/components/calender_view.tsx
@@ -2,46 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { Calendar } from '../components/ui/calendar';
 import { useAuth } from '@clerk/clerk-react';
 import { rspc } from '../utils/rspc';
+import { LinkLinks } from './links/link_lists';
+import ContentLoader from 'react-content-loader';
 
 function DailySummary({ date, total }: { date?: Date; total: number }) {
   return (
     <div className='container m-auto mt-3 flex  items-center justify-center rounded-md m-w-full text-[#111827]'>
       <div className='w-10 h-10 p-1 rounded-lg border border-gray-200 justify-center items-center inline-flex'>
         <div className='grow shrink basis-0 self-stretch p-1.5 bg-gray-100 rounded justify-center items-center inline-flex'>
-          <span>3</span>
+          <span>{date?.getDay()}</span>
         </div>
       </div>
       <div className='flex flex-col items-center m-auto justify-items-center'>
         <div className=''>
-          <p className='text-md'>Sep 20, 2023</p>
+          <p className='text-md'>{date?.toDateString()}</p>
         </div>
         <p className='text-xs'>
-          You added <a className='border border-gray-200'>0</a> links this day
+          You added <a className='border border-gray-200'>{total}</a> links this day
         </p>
       </div>
     </div>
   );
 }
 
-function DailyLink() {
-  return (
-    <div className='w-full h-14 relative rounded-lg border border-gray-200'>
-      <div className='p-1.5 left-[304px] top-[14px] absolute rounded-lg justify-center items-center gap-1.5 inline-flex'>
-        <div className='w-5 h-5 relative' />
-      </div>
-      <div className="left-[72px] top-[8px] absolute text-gray-900 text-sm font-normal font-['Inter'] leading-normal">
-        Scandinavia
-      </div>
-      <div className="left-[72px] top-[32px] absolute text-gray-500 text-xs font-normal font-['Inter'] leading-tight">
-        Denmark, Norway, Sweden
-      </div>
-      <div className='w-10 h-10 left-[16px] top-[10px] absolute rounded-lg border border-gray-200' />
-      <div className='w-8 h-8 p-1.5 left-[20px] top-[14px] absolute bg-gray-100 rounded justify-center items-center inline-flex'>
-        <div className='w-5 h-5 relative flex-col justify-start items-start flex' />
-      </div>
-    </div>
-  );
-}
 
 export function CalenderView() {
   const { getToken, isSignedIn, userId } = useAuth();
@@ -62,24 +45,49 @@ export function CalenderView() {
       refetch();
     }
   }, [date]);
-
+    
+  
   return (
-    <div className='right-sidebar flex h-full w-[280px] flex-col items-center justify-between border-l border-gray-200 bg-white'>
-      <div className='flex h-full w-full flex-auto flex-col overflow-hidden '>
-        <div className='flex h-full flex-col mb-2'>
-          <Calendar
-            mode='single'
-            selected={date}
-            onSelect={setDate}
-            className='rounded-md border-transparent text-gray-800'
-          />
-          <div className='relative flex w-full items-center justify-center mt-4 px-2'>
-            <div className='h-[1px] w-full rounded-full bg-gray-100' />
+      <div className='right-sidebar flex h-full w-[280px] flex-col items-center justify-between border-l border-gray-200 bg-white'>
+        <div className='flex h-full w-full flex-auto flex-col overflow-hidden '>
+          <div className='styled-scrollbar relative flex w-full flex-auto flex-col space-y-2 overflow-x-hidden overflow-y-auto px-2 pb-2 pt-2'>
+            <Calendar
+              mode='single'
+              selected={date}
+              onSelect={setDate}
+              className='rounded-md border-transparent text-gray-800'
+            />
+            <div className='relative flex w-full items-center justify-center mt-4 px-2 mb-4'>
+              <div className='h-[1px] w-full rounded-full bg-gray-100' />
+            </div>
+            { dateLinkStatus !== 'success' && (
+            <ContentLoader
+              speed={2}
+              width={400}
+              height={150}
+              viewBox='0 0 400 150'
+              backgroundColor='#f3f3f3'
+              foregroundColor='#ecebeb'
+              // {...props}
+            >
+              <circle cx='10' cy='20' r='8' />
+              <rect x='25' y='15' rx='5' ry='5' width='220' height='10' />
+              <circle cx='10' cy='50' r='8' />
+              <rect x='25' y='45' rx='5' ry='5' width='220' height='10' />
+              <circle cx='10' cy='80' r='8' />
+              <rect x='25' y='75' rx='5' ry='5' width='220' height='10' />
+              <circle cx='10' cy='110' r='8' />
+              <rect x='25' y='105' rx='5' ry='5' width='220' height='10' />
+            </ContentLoader>
+            )}
+            { dateLinkStatus === 'success' && 
+              (<>
+              <DailySummary date={date} total={dateLinks.length} />
+              <LinkLinks links={dateLinks} />
+              </>)
+            }
           </div>
-          <DailySummary date={date} total={0} />
-          {/* <LinkLists dateLinks={dateLinks} /> */}
         </div>
       </div>
-    </div>
-  );
-}
+      )
+  }
