a030ee6f5bba4bb8adffc0552423ce288e231c39
Frank Wang <frankwaang@outlook.com>
2023-09-29 11:31:49 -0500

    chore: fix search bar when screen size decrease

--- src/routes/tags.index.tsx
+++ src/routes/tags.index.tsx
@@ -34,26 +34,34 @@ export const route = new FileRoute('/tags/').createRoute({
     }
 
     return (
-    <div className='flex flex-col mx-auto justify-center'>
-      <h1 className='text-3xl font-semibold mb-3'>Tags</h1>
-      <div className='flex flex-row space-x-2'>
-      <Input placeholder='Search by tags...' />
-      <Button className='border-2 bg-gray-900'><Search /></Button>
-      </div>
-      <div className='flex wrap space-x-2 items-center justify-center mt-5'>
-        {allTags.map((tag) => (
-          <Badge
-            style={{
-              backgroundColor: `${tag.color}30`,
-              color: tag.color,
-              borderColor: `${tag.color}20`,
-            }}
-          >
-            {tag.name}
-          </Badge>
-        ))}
-      </div>
-    </div>
+<div className='flex flex-col mx-auto justify-center'>
+  <h1 className='text-3xl font-semibold mb-3'>Tags</h1>
+  <div className='flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2'>
+    <Input
+      className='w-full '
+      placeholder='Search by tags...'
+    />
+    <Button className='border-2 bg-gray-900 w-full md:w-auto'>
+      <Search />
+    </Button>
+  </div>
+  <div className='flex flex-wrap space-x-2 items-center justify-center mt-5'>
+    {allTags.map((tag) => (
+      <Badge
+        key={tag.id}
+        className='mb-2' // Add margin at the bottom for spacing
+        style={{
+          backgroundColor: `${tag.color}30`,
+          color: tag.color,
+          borderColor: `${tag.color}20`,
+        }}
+      >
+        {tag.name}
+      </Badge>
+    ))}
+  </div>
+</div>
+
     );
   },
 });
