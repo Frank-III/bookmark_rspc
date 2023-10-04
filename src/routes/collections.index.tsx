c8f98aad9ee4da44d76f9cf0ce47a1f75d8a8da3
Frank Wang <frankwaang@outlook.com>
2023-10-03 21:15:55 -0500

    wip: pagination

--- src/routes/collections.index.tsx
+++ src/routes/collections.index.tsx
@@ -7,24 +7,36 @@ import { rspc } from '../utils/rspc';
 import { CollectionPinned } from '../components/links/collection_lists';
 import { EditCllectionForm } from '../components/forms/edit_collection_forms';
 import { CardsSkeleton } from '../components/links/card_loader';
+import {CreateLinkArgs} from '../../bindings'
 
 export const route = new FileRoute('/collections/').createRoute({
   component: () => {
-    const { isLoading, data: collections } = rspc.useQuery([
-      'collections.getAllWithPinned',
-    ]);
-    if (isLoading) {
-      return <CardsSkeleton />;
-    }
+    // const { isLoading, data: collections } = rspc.useQuery([
+    //   'collections.getAllWithPinned',
+    // ]);
+    // if (isLoading) {
+    //   return <CardsSkeleton />;
+    // }
 
-    const collects = collections?.map((c) => {
-      const { pinnedBy, ...collection } = c;
-      return {
-        ...collection,
-        isPinned: pinnedBy.length > 0 ? true : false,
-      } as CollectionPinned;
-    });
+    // const collects = collections?.map((c) => {
+    //   const { pinnedBy, ...collection } = c;
+    //   return {
+    //     ...collection,
+    //     isPinned: pinnedBy.length > 0 ? true : false,
+    //   } as CollectionPinned;
+    // });
 
+    // const {mutate} = rspc.useMutation('links.create')
+
+    // const newLinks: CreateLinkArgs[] = Array(20).fill(0).map((_, i) => i).map((i) => {
+    //   return {
+    //     link_name: `test${i}`,
+    //     url: `https://test${i}.com`,
+    //     collection_id: Math.floor(Math.random() * 5),
+    //     tags: [Math.floor(Math.random() * 10)],
+    //     description: null
+    //   };
+    // })
     return (
       <>
         <div className='w-full bg-base-200'>
@@ -33,6 +45,7 @@ export const route = new FileRoute('/collections/').createRoute({
             <NewCollection>
               <Button variant='outline'>New Collection</Button>
             </NewCollection>
+            {/* <Button onClick={() => {newLinks.length ? mutate(newLinks?.pop()!) : ''}}>Create Links</Button> */}
             {/* <DropdownWithDialogItemsSolution2 collection={collects[0]}/> */}
           </div>
         </div>
---
+++ src/utils/useInfiniteLinkQuery.tsx
@@ -0,0 +1,6 @@
+import {client} from './rspc';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+
+
+
