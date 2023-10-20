424bab9e08ab73df4c7b98a37e951aa6dbc35250
Frank Wang <frankwaang@outlook.com>
2023-10-19 22:36:26 -0500

    add link component

--- client/src/components/buttons/full_link_card.tsx
+++ client/src/components/buttons/full_link_card.tsx
@@ -9,95 +9,68 @@ import {
 
 import { LinkWithTags } from '../../../bindings';
 import { Badge } from '../ui/badge';
-import { Archive, ArchiveX, ArrowRight, Calendar, Copy, Menu } from 'lucide-react';
+import { AlignJustify, Archive, ArchiveX, ArrowRight, Calendar, CheckCircle2, ChevronLeft, Copy, Menu, Tags } from 'lucide-react';
 import { LinkDropdown } from './link_popover';
 import { StyledButton } from './styled_button';
+import { cn } from '../../utils';
+import { TagBadge } from './tag_badge';
 
 export function FullLinkCard({ link }: { link: LinkWithTags }) {
   const { id, name, url, description, archived, collectionId, tags, createdAt} = link;
 
   return (
-    <div className='flex flex-col items-start w-full rounded-lg m-h-[300px] border-0 pb-0 shadow-small ring-1 ring-black/5 hover:border-2 hover:border-indigo-400 space-y-2'>
-      <div className='inline-flex'>
+    <div className='bg-white flex flex-col w-full rounded-lg m-h-[300px] border-0 pb-1 shadow-small ring-1 ring-black/5  hover:border-indigo-400'>
+    <div className='items-start space-y-1 py-0.5'>
+      <div className='inline-flex items-center'>
         <a 
           href={url}
           target='_blank'
           rel='noreferrer'
-          className='underline text-gray-800 font-light'
+          className='underline text-gray-800 font-medium ml-2'
         >
           {name}
         </a>
       </div>
-      <div className='w-full h[1-px] bg-gray-100' />
+      <div className='w-full h-[1px] bg-gray-300' />
+      <div className='flex flex-col space-y-1 gap-1 ml-1 text-sm text-gray-500'>
       {/* <div className='flex flex-col items-start'> */}
-      <div className='inline-flex space-x-2'>
-        <Calendar />
-        {createdAt}
+      <div className='inline-flex space-x-2 items-center gap-2'>
+        <Calendar size={16} />
+        {createdAt.slice(0, 10)}
       </div>
-      <div className='inline-flex space-x-2'>
-        <ArchiveX />
+      <div className='inline-flex space-x-2 items-center'>
+        <ArchiveX size={16} />
         {archived}
+        <Badge variant='outline' className={cn('ring-0 shadow-none px-1 py-0.5 rounded-full bg-opacity-20 font-normal', archived ? 'bg-rose-500 text-rose-500': 'bg-emerald-500 text-emerald-500')}>{archived ? 'false' : 'true'}</Badge>
       </div>
-      <div className='flex flex-row justify-between'>
-        <div className='inline-flex'>
-          <Copy />
-          <Archive />
-        </div>
-        <div className='inline-flex'>
-          
+      <div className='flex flex-row space-x-2 items-center'>
+        <Tags size={16}/>
+        <div className='inline-flex overflow-x-auto scrollbar-none space-x-[5px]'>
+        {tags.map((tag) => (<TagBadge className='py-0.5 mb-1' tag={tag}/>))}
         </div>
       </div>
+      <div className={cn('inline-flex space-x-2 items-center wrap gap-2', !description && 'text-gray-400')}>
+        <AlignJustify size={16}/>
+        {description}
       </div>
-
-    // <Card
-    //   className='w-full rounded-lg m-h-[200px] border-0 pb-0 shadow-small ring-1 ring-black/5 hover:border-2 hover:border-indigo-400 '
-    //   key={id}
-    // >
-    //   <CardHeader className='p-3 text-gray-700 font-medium'>
-    //     <CardTitle>{name}</CardTitle>
-    //     <CardDescription>{description || 'None'}</CardDescription>
-    //   </CardHeader>
-    //   <CardContent className='p-2'>
-    //     <div className='h-[1px] w-full rounded-full bg-gray-100' />
-    //   </CardContent>
-    //   <div
-    //     className='flex flex-row overflow-x-auto px-1 scrollbar-none' // Add 'overflow-x-auto' class for horizontal scroll
-    //     style={{ paddingBottom: '10px' }} // Add some padding for the button
-    //   >
-    //     {link.tags.map((tag) => (
-    //       <Badge
-    //         key={tag.id}
-    //         style={{
-    //           backgroundColor: `${tag.color}30`,
-    //           color: tag.color,
-    //           borderColor: `${tag.color}20`,
-    //           marginRight: '8px', // Add some space between badges
-    //         }}
-    //       >
-    //         {tag.name}
-    //       </Badge>
-    //     ))}
-    //   </div>
-    //   <div className='h-[1px] w-full rounded-full bg-gray-100' />
-    //   <div className='mt-2 flex w-full items-center justify-end space-x-1 mb-0'>
-    //     <LinkDropdown link={link}>
-    //       <button className='bg-white text-gray-700  ring-1 ring-black/5 shadow-small hover:bg-gray-50 disabled:text-gray-400 disabled:hover:bg-white auto shrink-0 rounded-lg  text-sm  relative flex flex-row items-center justify-center space-x-1 font-medium transition disabled:cursor-not-allowed h-6 px-0 w-6 '>
-    //         <Menu size={20} />
-    //       </button>
-    //     </LinkDropdown>
-    //     <a
-          // href={url}
-          // target='_blank'
-          // rel='noreferrer'
-          // className='h-6 w-6 ' // Position the button at the bottom-right
-    //     >
-    //       <StyledButton className='h-6 px-0 w-6'>
-    //         <ArrowRight color={'black'} size={20} />
-    //       </StyledButton>
-    //     </a>
-    //   </div>
-    // </Card>
-  );
+      </div>
+    </div>
+    <div className='flex flex-row justify-between text-gray-600 mx-1 mt-2'>
+      <div className='inline-flex space-x-1'>
+        <button className='hover:bg-gray-200 p-1 rounded-md hover:text-gray-600'>
+        <ChevronLeft size={16}/>
+        </button>
+      </div>
+      <div className='inline-flex space-x-1'>
+        <button className='hover:bg-gray-200 p-1 rounded-md hover:text-gray-600'>
+          <Copy size={16}/>
+        </button>
+        <button className='hover:bg-gray-200 p-1 rounded-md hover:text-gray-600'>
+          <CheckCircle2 size={16}/>
+        </button>
+      </div>
+    </div>
+  </div>);
 }
 
 //wrap
