f1ce03a3ceed4dee3165aa36e321b14f28383a13
Frank Wang <frankwaang@outlook.com>
2023-11-05 18:54:33 -0600

    init refactor: support share collection(wip)

--- client/src/utils/rspc.ts
+++ client/src/utils/rspc.ts
@@ -22,7 +22,10 @@ const fetch = async (input: RequestInfo | URL, init?: RequestInit) => {
 // You must provide the generated types as a generic and create a transport (in this example we are using HTTP Fetch) so that the client knows how to communicate with your API.
 const client = createClient<Procedures>({
   // Refer to the integration your using for the correct transport.
-  transport: new FetchTransport('http://localhost:9000/rspc', fetch as unknown as typeof globalThis.fetch),
+  transport: new FetchTransport(
+    'http://localhost:9000/rspc',
+    fetch as unknown as typeof globalThis.fetch,
+  ),
 });
 
 const queryClient = new QueryClient({
